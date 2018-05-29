c3bdb0e45e4486560bb276d36588d9c561ee01c8
raynardj <raynard@rasenn.com>
2018-05-29 01:06:53 -0400

    v0.0.3 with 2 layers in GRU

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import torch
 
-BS = 16# Batch size
+BS = 8# Batch size
 
 VOCAB_SEQ_IN = 3000
 VOCAB_SEQ_OUT = 3000
@@ -10,14 +10,16 @@ EOS_TOKEN = 1
 
 LR = 5e-3
 HIDDEN_SIZE = 512
+NB_LAYER = 2
 
-VERSION = "0.0.2"
-# "0.0.1" chars
-# "0.0.2" token
+VERSION = "0.0.3"
+# "0.0.1" chars hidden =256
+# "0.0.2" token hidden =512
+# "0.0.3" layer=2 hidden =512
 
 CUDA = torch.cuda.is_available()
 
-CN_SEG = True
+CN_SEG = False
 
 if CN_SEG:
     DICT_IN = "/data/dict/chat_vocab_in.csv"
@@ -27,3 +29,5 @@ else:
     DICT_IN = "/data/dict/chat_vocab_in.csv"
     DICT_OUT = "/data/dict/chat_vocab_out.csv"
     SEQ_DIR = "/data/chat/xhj_seq_char.npy"
+    
+MAX_LEN = 20
--- models.py
+++ models.py
@@ -2,12 +2,16 @@ from torch import nn
 from constants import *
 import torch
 
+# GRU
+
 class EncoderRNN_GRU(nn.Module):
     def __init__(self, input_size, hidden_size, n_layers=1):
         super(EncoderRNN_GRU, self).__init__()
+        self.n_layers = n_layers
         self.hidden_size = hidden_size
         self.embedding = nn.Embedding(input_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True, num_layers=n_layers)
+        self.cuda_ = CUDA
         
     def forward(self, input_, hidden):
         output, hidden = self.gru(self.embedding(input_), hidden)
@@ -15,8 +19,8 @@ class EncoderRNN_GRU(nn.Module):
 
     # TODO: other inits
     def initHidden(self, batch_size):
-        en_hidden = torch.zeros(1, batch_size, self.hidden_size)
-        if CUDA:
+        en_hidden = torch.zeros(self.n_layers, batch_size, self.hidden_size)
+        if self.cuda_:
             en_hidden = en_hidden.cuda()
         return en_hidden
     
@@ -27,7 +31,8 @@ class DecoderRNN_GRU(nn.Module):
         self.gru = nn.GRU(hidden_size, hidden_size, batch_first=True, num_layers=n_layers)
         # TODO use transpose of embedding
         self.out = nn.Linear(hidden_size, output_size)
-        self.sm = nn.LogSoftmax()
+        self.sm = nn.LogSoftmax(dim=-1)
+        self.cuda_ = CUDA
         
     def forward(self, input_, hidden):
         emb = self.embedding(input_).unsqueeze(1)
@@ -38,6 +43,51 @@ class DecoderRNN_GRU(nn.Module):
     
     def initInput(self,batch_size):
         decoder_input = torch.LongTensor([SOS_TOKEN]*batch_size)
-        if CUDA:
+        if self.cuda_:
+            decoder_input = decoder_input.cuda()
+        return decoder_input
+    
+# LSTM
+
+class EncoderRNN_LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, n_layers=1):
+        super(EncoderRNN_LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=True, num_layers=n_layers)
+        self.cuda_ = CUDA
+        
+    def forward(self, input_, hidden):
+        print(self.embedding(input_).size())
+        output, hidden = self.lstm(self.embedding(input_), hidden)
+        return output, hidden
+
+    # TODO: other inits
+    def initHidden(self, batch_size):
+        en_hidden = torch.zeros(1, batch_size, self.hidden_size)
+        if self.cuda_:
+            en_hidden = en_hidden.cuda()
+        return en_hidden
+    
+class DecoderRNN_LSTM(nn.Module):
+    def __init__(self, hidden_size, output_size, n_layers=1):
+        super(DecoderRNN_LSTM, self).__init__()
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, batch_first=True, num_layers=n_layers)
+        # TODO use transpose of embedding
+        self.out = nn.Linear(hidden_size, output_size)
+        self.sm = nn.LogSoftmax(dim=-1)
+        self.cuda_ = CUDA
+        
+    def forward(self, input_, hidden):
+        emb = self.embedding(input_).unsqueeze(1)
+        # NB: Removed relu
+        res, hidden = self.lstm(emb, hidden)
+        output = self.sm(self.out(res[:,0]))
+        return output, hidden
+    
+    def initInput(self,batch_size):
+        decoder_input = torch.LongTensor([SOS_TOKEN]*batch_size)
+        if self.cuda_:
             decoder_input = decoder_input.cuda()
         return decoder_input
